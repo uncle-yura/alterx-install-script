384e8e507b57031bb0178448ddfc2d9bf360c442
uncle-yura <uncle-yura@tuta.io>
2021-09-22 10:12:31 +0300

    Added awlsim install command.

--- install.sh
+++ install.sh
@@ -82,6 +82,7 @@ echo "********* Clone interface *********"
 git clone https://github.com/uncle-yura/alterx.git /home/$USER/alterx && echo Complete 1 || critical_fail 1 clone
 python /home/$USER/alterx/setup.py && echo Complete 2 || critical_fail 2 clone
 git clone https://github.com/uncle-yura/awlsim.git /home/$USER/awlsim && echo Complete 3 || critical_fail 3 clone
+python /home/$USER/awlsim/setup.py install && echo Complete 4 || critical_fail 4 clone
 
 keyboard:
 echo "********* Install alterx keyboard *********"
